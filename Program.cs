b43d03c72955878cfa1a382fb049478b32784b95
Matheus <matheus_karling@estudante.sesisenai.org.br>
2026-06-11 20:02:16 -0300

    q-1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿Console.Write(" Informe sua idade: ");
+if (int.Parse(Console.ReadLine()!) >= 16){
+    Console.WriteLine("Entrada permitida");
+} else{
+    Console.WriteLine("Entrada permitida somente com responsável.");
+}    
